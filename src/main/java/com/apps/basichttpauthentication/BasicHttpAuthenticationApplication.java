2612833eca09f081da4595bf2403c32cddbd1c8a
williamfisher9 <fisher.a.william@gmail.com>
2024-01-28 22:39:43 -0600

    initial project code

---
+++ src/main/java/com/apps/basichttpauthentication/BasicHttpAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.apps.basichttpauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicHttpAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicHttpAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps/basichttpauthentication/BasicHttpAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.basichttpauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicHttpAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
